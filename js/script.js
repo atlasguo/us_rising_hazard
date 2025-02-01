41e6390213798f80bebb93672d4fa5e0660ddc9f
atlasguo <chenxiao.atlas.guo@gmail.com>
2025-02-01 21:48:13 +0800

    update (buttons)

--- js/script.js
+++ js/script.js
@@ -25,14 +25,20 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/wid
 
     // Create zoom in and zoom out buttons
     const zoomInButton = document.createElement("button");
-    zoomInButton.innerHTML = "+";
-    zoomInButton.className = "zoom-button";
+    zoomInButton.className = "zoom-button mapButton";
     zoomInButton.id = "zoomInButton";
+    const zoomInIcon = document.createElement("i");
+    zoomInIcon.className = "fas fa-plus";
+    zoomInIcon.id = "zoomInIcon";
+    zoomInButton.appendChild(zoomInIcon);
 
     const zoomOutButton = document.createElement("button");
-    zoomOutButton.innerHTML = "-";
-    zoomOutButton.className = "zoom-button";
+    zoomOutButton.className = "zoom-button mapButton";
     zoomOutButton.id = "zoomOutButton";
+    const zoomOutIcon = document.createElement("i");
+    zoomOutIcon.className = "fas fa-minus";
+    zoomOutIcon.id = "zoomOutIcon";
+    zoomOutButton.appendChild(zoomOutIcon);
 
     // Add event listeners for zoom buttons
     zoomInButton.addEventListener("click", function () {
@@ -84,14 +90,20 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/wid
                         features: results.features,
                         location: point
                     });
+                    // Increase the height of the popup window
+                    view.popup.viewModel.container.style.height = "1.5em";
                 }
             });
+
+            // Hide the infoDialog
+            const infoDialog = document.getElementById("infoDialog");
+            infoDialog.style.display = "none";
         }
     });
 
     // Create home button
     const homeButton = document.createElement("button");
-    homeButton.className = "zoom-button";
+    homeButton.className = "zoom-button mapButton";
     homeButton.id = "homeButton";
     const homeIcon = document.createElement("i");
     homeIcon.className = "fas fa-home";
@@ -111,7 +123,7 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/wid
 
     // Create info button
     const infoButton = document.createElement("button");
-    infoButton.className = "zoom-button";
+    infoButton.className = "zoom-button mapButton";
     infoButton.id = "infoButton";
     const infoIcon = document.createElement("i");
     infoIcon.className = "fas fa-info-circle";
@@ -120,25 +132,29 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/wid
 
     // Add event listener for info button
     infoButton.addEventListener("click", function () {
-        const infoDialog = document.createElement("div");
-        infoDialog.id = "infoDialog";
+        const infoDialog = document.getElementById("infoDialog");
+        const legendContent = document.getElementById("legendContent");
+        const legendContainer = document.getElementById("legendContainer");
+        const toggleButton = document.getElementById("toggleButton");
 
-        const infoContent = document.createElement("div");
-        infoContent.innerHTML = "Welcome to the web map of <b>The United States of Rising Hazards</b>! The map is designed by Atlas Guo (2025), CartoGuophy.com";
-        infoDialog.appendChild(infoContent);
-
-        const closeButton = document.createElement("button");
-        closeButton.innerHTML = "Close";
-        closeButton.addEventListener("click", function () {
-            document.body.removeChild(infoDialog);
-        });
-        infoDialog.appendChild(closeButton);
-
-        document.body.appendChild(infoDialog);
+        if (infoDialog.style.display === "block") {
+            infoDialog.style.display = "none";
+        } else {
+            infoDialog.style.display = "block";
+            // Collapse the legend
+            legendContent.style.display = "none";
+            legendContainer.style.maxHeight = "50px"; // Collapsed height
+            legendContainer.style.width = "250px"; // Collapsed width
+            toggleButton.innerHTML = "<i class='fas fa-chevron-up'></i>";
+        }
     });
 
     // Add info button to the view
     view.ui.add(infoButton, "manual");
+
+    // Display info dialog initially
+    const initialInfoDialog = document.getElementById("infoDialog");
+    initialInfoDialog.style.display = "block"; // Ensure the dialog is displayed
 
     // Define the Feature Layer URL
     const featureLayerUrl = "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/usbr_point/FeatureServer";
@@ -174,12 +190,12 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/wid
                         type: "size",
                         valueExpression: "$view.scale",
                         stops: [
-                            { value: 36978595, size: 2 },
-                            { value: 18489298, size: 4 },
-                            { value: 9244649, size: 8 },
-                            { value: 4622324, size: 16 },
-                            { value: 2311162, size: 32 },
-                            { value: 577791, size: 64 }
+                            { value: 36978595, size: 1.8 },
+                            { value: 18489298, size: 3.6 },
+                            { value: 9244649, size: 7.2 },
+                            { value: 4622324, size: 14.4 },
+                            { value: 2311162, size: 28.8 },
+                            { value: 577791, size: 57.6 },
                         ]
                     }
                 ]
@@ -234,114 +250,187 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/wid
         popupTemplate: {
             title: "Risk Score in this 1K-sq-mi Hexagon",
             content: [
-                { type: "text", text: "{expression/score7} Heat Wave: {score7}" },
-                { type: "text", text: "{expression/score8} Hurricane: {score8}" },
-                { type: "text", text: "{expression/score14} Tornado: {score14}" },
-                { type: "text", text: "{expression/score13} Strong Wind: {score13}" },
-                { type: "text", text: "{expression/score11} Lightning: {score11}" },
-                { type: "text", text: "{expression/score6} Hail: {score6}" },
-                { type: "text", text: "{expression/score9} Ice Storm: {score9}" },
-                { type: "text", text: "{expression/score18} Winter Weather: {score18}" },
-                { type: "text", text: "{expression/score3} Cold Wave: {score3}" },
-                { type: "text", text: "{expression/score1} Avalanche: {score1}" },
-                { type: "text", text: "{expression/score12} Riverine Flooding: {score12}" },
-                { type: "text", text: "{expression/score2} Coastal Flooding: {score2}" },
-                { type: "text", text: "{expression/score10} Landslide: {score10}" },
-                { type: "text", text: "{expression/score15} Tsunami: {score15}" },
-                { type: "text", text: "{expression/score5} Earthquake: {score5}" },
-                { type: "text", text: "{expression/score16} Volcanic Activity: {score16}" },
-                { type: "text", text: "{expression/score17} Wildfire: {score17}" },
-                { type: "text", text: "{expression/score4} Drought: {score4}" }
+                { type: "text", text: "{expression/color7} Heat Wave: {expression/score7}<br>{expression/color8} Hurricane: {expression/score8}<br>{expression/color14} Tornado: {expression/score14}<br>{expression/color13} Strong Wind: {expression/score13}<br>{expression/color11} Lightning: {expression/score11}<br>{expression/color6} Hail: {expression/score6}<br>{expression/color9} Ice Storm: {expression/score9}<br>{expression/color18} Winter Weather: {expression/score18}<br>{expression/color3} Cold Wave: {expression/score3}<br>{expression/color1} Avalanche: {expression/score1}<br>{expression/color12} Riverine Flooding: {expression/score12}<br>{expression/color2} Coastal Flooding: {expression/score2}<br>{expression/color10} Landslide: {expression/score10}<br>{expression/color15} Tsunami: {expression/score15}<br>{expression/color5} Earthquake: {expression/score5}<br>{expression/color16} Volcanic Activity: {expression/score16}<br>{expression/color17} Wildfire: {expression/score17}<br>{expression/color4} Drought: {expression/score4}" }
             ],
             expressionInfos: [
                 {
                     name: "score7",
                     title: "Heat Wave",
-                    expression: getColorExpression("score7")
+                    expression: "Round($feature.score7, 1)"
                 },
                 {
                     name: "score8",
                     title: "Hurricane",
-                    expression: getColorExpression("score8")
+                    expression: "Round($feature.score8, 1)"
                 },
                 {
                     name: "score14",
                     title: "Tornado",
-                    expression: getColorExpression("score14")
+                    expression: "Round($feature.score14, 1)"
                 },
                 {
                     name: "score13",
                     title: "Strong Wind",
-                    expression: getColorExpression("score13")
+                    expression: "Round($feature.score13, 1)"
                 },
                 {
                     name: "score11",
                     title: "Lightning",
-                    expression: getColorExpression("score11")
+                    expression: "Round($feature.score11, 1)"
                 },
                 {
                     name: "score6",
                     title: "Hail",
-                    expression: getColorExpression("score6")
+                    expression: "Round($feature.score6, 1)"
                 },
                 {
                     name: "score9",
                     title: "Ice Storm",
-                    expression: getColorExpression("score9")
+                    expression: "Round($feature.score9, 1)"
                 },
                 {
                     name: "score18",
                     title: "Winter Weather",
-                    expression: getColorExpression("score18")
+                    expression: "Round($feature.score18, 1)"
                 },
                 {
                     name: "score3",
                     title: "Cold Wave",
-                    expression: getColorExpression("score3")
+                    expression: "Round($feature.score3, 1)"
                 },
                 {
                     name: "score1",
                     title: "Avalanche",
-                    expression: getColorExpression("score1")
+                    expression: "Round($feature.score1, 1)"
                 },
                 {
                     name: "score12",
                     title: "Riverine Flooding",
-                    expression: getColorExpression("score12")
+                    expression: "Round($feature.score12, 1)"
                 },
                 {
                     name: "score2",
                     title: "Coastal Flooding",
-                    expression: getColorExpression("score2")
+                    expression: "Round($feature.score2, 1)"
                 },
                 {
                     name: "score10",
                     title: "Landslide",
-                    expression: getColorExpression("score10")
+                    expression: "Round($feature.score10, 1)"
                 },
                 {
                     name: "score15",
                     title: "Tsunami",
-                    expression: getColorExpression("score15")
+                    expression: "Round($feature.score15, 1)"
                 },
                 {
                     name: "score5",
                     title: "Earthquake",
-                    expression: getColorExpression("score5")
+                    expression: "Round($feature.score5, 1)"
                 },
                 {
                     name: "score16",
                     title: "Volcanic Activity",
-                    expression: getColorExpression("score16")
+                    expression: "Round($feature.score16, 1)"
                 },
                 {
                     name: "score17",
                     title: "Wildfire",
-                    expression: getColorExpression("score17")
+                    expression: "Round($feature.score17, 1)"
                 },
                 {
                     name: "score4",
                     title: "Drought",
+                    expression: "Round($feature.score4, 1)"
+                },
+                {
+                    name: "color7",
+                    title: "Heat Wave Color",
+                    expression: getColorExpression("score7")
+                },
+                {
+                    name: "color8",
+                    title: "Hurricane Color",
+                    expression: getColorExpression("score8")
+                },
+                {
+                    name: "color14",
+                    title: "Tornado Color",
+                    expression: getColorExpression("score14")
+                },
+                {
+                    name: "color13",
+                    title: "Strong Wind Color",
+                    expression: getColorExpression("score13")
+                },
+                {
+                    name: "color11",
+                    title: "Lightning Color",
+                    expression: getColorExpression("score11")
+                },
+                {
+                    name: "color6",
+                    title: "Hail Color",
+                    expression: getColorExpression("score6")
+                },
+                {
+                    name: "color9",
+                    title: "Ice Storm Color",
+                    expression: getColorExpression("score9")
+                },
+                {
+                    name: "color18",
+                    title: "Winter Weather Color",
+                    expression: getColorExpression("score18")
+                },
+                {
+                    name: "color3",
+                    title: "Cold Wave Color",
+                    expression: getColorExpression("score3")
+                },
+                {
+                    name: "color1",
+                    title: "Avalanche Color",
+                    expression: getColorExpression("score1")
+                },
+                {
+                    name: "color12",
+                    title: "Riverine Flooding Color",
+                    expression: getColorExpression("score12")
+                },
+                {
+                    name: "color2",
+                    title: "Coastal Flooding Color",
+                    expression: getColorExpression("score2")
+                },
+                {
+                    name: "color10",
+                    title: "Landslide Color",
+                    expression: getColorExpression("score10")
+                },
+                {
+                    name: "color15",
+                    title: "Tsunami Color",
+                    expression: getColorExpression("score15")
+                },
+                {
+                    name: "color5",
+                    title: "Earthquake Color",
+                    expression: getColorExpression("score5")
+                },
+                {
+                    name: "color16",
+                    title: "Volcanic Activity Color",
+                    expression: getColorExpression("score16")
+                },
+                {
+                    name: "color17",
+                    title: "Wildfire Color",
+                    expression: getColorExpression("score17")
+                },
+                {
+                    name: "color4",
+                    title: "Drought Color",
                     expression: getColorExpression("score4")
                 }
             ]
@@ -352,9 +441,9 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/wid
         return `
             var score = $feature.${field};
             return When(
-                score >= 80, "🟥", 
-                score >= 60, "🟧",  
-                score >= 40, "🟨",  
+                score >= 90, "🟥", 
+                score >= 70, "🟧",  
+                score >= 50, "🟨",  
                 "🟩"               
             );
         `;
@@ -366,9 +455,11 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/wid
     // Watch for scale changes and update opacity
     view.watch("scale", function (newValue) {
         if (newValue <= 4622324) {
-            hexFeatureLayer.opacity = 0.2; // Set opacity to 30% when scale is 4622324 or lower
+            hexFeatureLayer.opacity = 0.2;
+        } else if (newValue <= 9244649) {
+            hexFeatureLayer.opacity = 0.05;
         } else {
-            hexFeatureLayer.opacity = 0.01; // Set opacity back to 10% for higher scales
+            hexFeatureLayer.opacity = 0.01;
         }
     });
 
@@ -425,7 +516,8 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/wid
     // Add toggle button to the legend header
     const toggleButton = document.createElement("button");
     toggleButton.id = "toggleButton";
-    toggleButton.innerHTML = "Show";
+    toggleButton.innerHTML = "<i class='fas fa-chevron-up'></i>";
+
     legendHeader.appendChild(toggleButton);
 
     legendContainer.appendChild(legendHeader);
@@ -438,16 +530,20 @@ require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/wid
     legendContainer.appendChild(legendContent);
 
     legendHeader.addEventListener("click", function () {
+        const infoDialog = document.getElementById("infoDialog");
+        if (infoDialog.style.display === "block") {
+            infoDialog.style.display = "none";
+        }
         if (legendContent.style.display === "none") {
             legendContent.style.display = "block";
             legendContainer.style.maxHeight = "500px"; // Expanded height
             legendContainer.style.width = "500px"; // Expanded width
-            toggleButton.innerHTML = "Hide";
+            toggleButton.innerHTML = "<i class='fas fa-chevron-down'></i>";
         } else {
             legendContent.style.display = "none";
             legendContainer.style.maxHeight = "50px"; // Collapsed height
             legendContainer.style.width = "250px"; // Collapsed width
-            toggleButton.innerHTML = "Show";
+            toggleButton.innerHTML = "<i class='fas fa-chevron-up'></i>";
         }
     });
 
